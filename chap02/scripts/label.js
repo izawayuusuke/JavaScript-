ad4ad6bdec2a32e7d33837e974a48e1749c8c4f4
izawa yuusuke <yuu05020@gmail.com>
2020-07-16 21:07:43 +0900

    ラベル構文

---
+++ chap02/scripts/label.js
@@ -0,0 +1,10 @@
+ninety_nine: for (var i = 1; i < 10; i++) {
+  for (var j = 1; j < 10; j++) {
+    var k = i * j;
+    if (k > 30) {
+      break ninety_nine;
+    }
+    document.write(k + "&nbsp");
+  }
+  document.write("<br />");
+}
