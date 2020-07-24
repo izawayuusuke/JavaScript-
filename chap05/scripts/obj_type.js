7c1ce4af6b38e48ff83475f7fffd7488006c2f96
izawa yuusuke <yuu05020@gmail.com>
2020-07-24 13:32:40 +0900

    参照しているプロトタイプを確認する

--- chap05/scripts/obj_type.js
+++ chap05/scripts/obj_type.js
@@ -5,6 +5,5 @@ Hamster.prototype = new Animal();
 
 var a = new Animal();
 var h = new Hamster();
-console.log(a.constructor === Animal);
-console.log(h instanceof Animal);
-console.log(h instanceof Hamster);
+console.log(Hamster.prototype.isPrototypeOf(h));
+console.log(Animal.prototype.isPrototypeOf(h));
