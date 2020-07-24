271d55a3c517fc1a39d6f7010b15171017fb7b20
izawa yuusuke <yuu05020@gmail.com>
2020-07-24 13:28:43 +0900

    元となるコンストラクターを取得する

---
+++ chap05/scripts/obj_type.js
@@ -0,0 +1,10 @@
+// Animalクラスと、これを継承したHamsterクラスを準備
+var Animal = function () {};
+var Hamster = function () {};
+Hamster.prototype = new Animal();
+
+var a = new Animal();
+var h = new Hamster();
+console.log(a.constructor === Animal);
+console.log(h.constructor === Animal);
+console.log(h.constructor === Hamster);
