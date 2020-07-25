79afbccc334ddc6eadb1f0ebe2669bee47915e9a
izawa yuusuke <yuu05020@gmail.com>
2020-07-25 23:48:23 +0900

    素数を求めるジェネレーター

---
+++ chap05/scripts/gen_prime.js
@@ -0,0 +1,31 @@
+// 素数を求めるためのジェネレーター
+function* genPrimes() {
+  let num = 2; //素数の関数値
+  // 2から順に素数判定し、素数の場合だけyield (無限ループ)
+  while (true) {
+    if (isPrime(num)) {
+      yield num;
+    }
+    num++;
+  }
+
+  // 引数valueが素数かどうかを判定
+  function isPrime(value) {
+    let prime = true; // 素数かどうかを表すフラグ
+    for (let i = 2; i <= Math.floor(Math.sqrt(value)); i++) {
+      if (value % i === 0) {
+        prime = false; // 割り切れたら素数でない
+        break;
+      }
+    }
+    return prime;
+  }
+}
+
+for (let value of genPrimes()) {
+  // 素数が101以上になったら終了(これがないと無限ループになるので注意！)
+  if (value > 100) {
+    break;
+  }
+  console.log(value);
+}
