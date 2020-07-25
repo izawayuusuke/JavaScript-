086392e96dd27a822dd607222f67fa67ef4cc185
izawa yuusuke <yuu05020@gmail.com>
2020-07-25 21:18:14 +0900

    プライベートメンバーを定義する - Symbol -

---
+++ chap05/scripts/lib/MyApp.js
@@ -0,0 +1,15 @@
+const SECRET_VALUE = Symbol();
+
+export default class {
+  constructor(secret) {
+    this.hoge = "hoge";
+    this.foo = "foo";
+    // SECRET_VALUEプロパティも初期化
+    this[SECRET_VALUE] = secret;
+  }
+
+  // SECRET_VALUEプロパティを利用したメソッド
+  checkValue(secret) {
+    return this[SECRET_VALUE] === secret;
+  }
+}
