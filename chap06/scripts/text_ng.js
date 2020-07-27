afeb80c9e966e6700de08dd354e5d7f3d30f329e
izawa yuusuke <yuu05020@gmail.com>
2020-07-27 16:45:21 +0900

    innerHTMLプロパティのクロスサイトスクリプティング(XSS)脆弱性

---
+++ chap06/scripts/text_ng.js
@@ -0,0 +1,16 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    // ボタンクリック時に挨拶メッセージを反映
+    document.getElementById("btn").addEventListener(
+      "click",
+      function () {
+        var name = document.getElementById("name");
+        var result = document.getElementById("result");
+        result.innerHTML = "こんにちは、" + name.value + "さん！";
+      },
+      false
+    );
+  },
+  false
+);
