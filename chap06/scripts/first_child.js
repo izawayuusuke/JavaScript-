c4df2b1604f4b709387555d132797a9d8a87caeb
izawa yuusuke <yuu05020@gmail.com>
2020-07-27 15:02:16 +0900

    firstChild／nextSiblingプロパティで書き換える

---
+++ chap06/scripts/first_child.js
@@ -0,0 +1,11 @@
+var s = document.getElementById("food");
+// <select>要素の最初の子ノードを取得
+var child = s.firstChild;
+// 子ノードが存在する間、ループを繰り返す
+while (child) {
+  if (child.nodeType === 1) {
+    console.log(child.value);
+  }
+  // 次の子ノード（弟ノードを取得）
+  child = child.nextSibling;
+}
