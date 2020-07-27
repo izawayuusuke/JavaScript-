709bdeb1ee56cc7df2d011db6929c0742792bb88
izawa yuusuke <yuu05020@gmail.com>
2020-07-27 17:02:09 +0900

    入力ボックス／選択ボックスの値を取得する

---
+++ chap06/scripts/text_select.js
@@ -0,0 +1,15 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    document.getElementById("btn") /
+      addEventListener(
+        "click",
+        function () {
+          var name = document.getElementById("name");
+          console.log(name.value);
+        },
+        false
+      );
+  },
+  false
+);
