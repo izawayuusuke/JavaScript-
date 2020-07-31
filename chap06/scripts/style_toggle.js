44985020af1a65bc6ed8e7c6799ba8c458d0c3b3
izawa yuusuke <yuu05020@gmail.com>
2020-07-31 19:59:54 +0900

    スタイルクラスを着脱する

---
+++ chap06/scripts/style_toggle.js
@@ -0,0 +1,16 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    var elem = document.getElementById("elem");
+
+    // クリックしたタイミングで背景色を変更
+    elem.addEventListener(
+      "click",
+      function () {
+        this.className = this.className === "highlight" ? "" : "highlight";
+      },
+      false
+    );
+  },
+  false
+);
