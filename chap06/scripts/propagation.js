e4b0015d537381dde1d20b50defe8c580825cbd4
izawa yuusuke <yuu05020@gmail.com>
2020-08-01 14:09:49 +0900

    イベントの伝播

---
+++ chap06/scripts/propagation.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // <a id="inner">要素のclickイベントリスナー
+  document.getElementById("inner").addEventListener(
+    "click",
+    function (e) {
+      window.alert("#innerリスナーが発生しました。");
+    },
+    false
+  );
+
+  document.getElementById("inner").addEventListener(
+    "click",
+    function (e) {
+      window.alert("#innerリスナー2が発生しました。");
+    },
+    false
+  );
+
+  // <div id="outer">要素のclickイベントリスナー
+  document.getElementById("outer").addEventListener(
+    "click",
+    function (e) {
+      window.alert("#outerリスナーが発生しました。");
+    },
+    false
+  );
+});
