5f5f6890e7639522b8414bb2b128a851231c94fb
izawa yuusuke <yuu05020@gmail.com>
2020-08-01 21:42:03 +0900

    確認ダイアログを表示する

---
+++ chap07/scripts/confirm.js
@@ -0,0 +1,15 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    document.getElementById("fm").addEventListener(
+      "submit",
+      function (e) {
+        if (!window.confirm("ページを送信しても良いですか？")) {
+          e.preventDefault();
+        }
+      },
+      false
+    );
+  },
+  false
+);
