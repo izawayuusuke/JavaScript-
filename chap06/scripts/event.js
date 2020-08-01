f9f375fba24cc06686d93c83ae48737aa606404d
izawa yuusuke <yuu05020@gmail.com>
2020-08-01 13:49:29 +0900

    イベントオブジェクトの基本

---
+++ chap06/scripts/event.js
@@ -0,0 +1,15 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    document.getElementById("btn").addEventListener(
+      "click",
+      function (e) {
+        var target = e.target;
+        console.log("発生元：" + target.nodeName + "/" + target.id);
+        console.log("種類：" + e.type);
+      },
+      false
+    );
+  },
+  false
+);
