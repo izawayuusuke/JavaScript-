77b7b5356821c6b0b7727a096eb1b28aeba76247
izawa yuusuke <yuu05020@gmail.com>
2020-08-05 19:16:00 +0900

    Polyfillライブラリを有効化する

---
+++ chap08/babel/src/polyfill.js
@@ -0,0 +1,30 @@
+// Mapオブジェクトに値を追加
+let m = new Map();
+m.set("dog", "ワンワン");
+m.set("cat", "ニャー");
+m.set("mouse", "チュー");
+
+console.log(m.size);
+console.log(m.get("dog"));
+console.log(m.has("cat"));
+
+// キーを順に取得
+for (let key of m.keys()) {
+  console.log(key);
+}
+// 値を順に取得
+for (let value of m.values()) {
+  console.log(value);
+}
+// キー／値を順に取得
+for (let [key, value] of m) {
+  console.log(key + "：" + value);
+}
+
+// キーdogを削除
+m.delete("dog");
+console.log(m.size);
+
+// すべてのキー／値を破棄
+m.clear();
+console.log(m.size);
